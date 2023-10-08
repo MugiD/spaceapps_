5c1e987783dfd1c9e618002f92bde0d240548056
noodlix <zhibeck271205@gmail.com>
2023-10-08 13:03:32 +0600

    chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Hero from '@/components/Hero'
+import Chat from '@/components/Chat'
+import Image from "next/image";
 
+import {  ArrowLongRightIcon,
+  
+} from "@heroicons/react/24/outline";
 export default function Home() {
   return (
-    <Hero />
+    <>
+    <Chat/>
+    </>
   )
 }
